a727c57424029aeeafef14bd196a25a18daa2a0f
Uladzislau Vensko <ulad.vensko@gmail.com>
2019-09-15 21:26:23 +0300

    Added JS build

--- gulpfile.js
+++ gulpfile.js
@@ -5,17 +5,22 @@ const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 const htmlhint = require('gulp-htmlhint');
 const prettyHtml = require('gulp-pretty-html');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
 
 // Config directories
 const dirs = {
   src: {
     html: 'src/*.html',
+    js: 'src/js/**/*.js'
   },
   dist: {
     html: 'dist/',
+    js: 'dist/js/'
   },
   watch: {
     html: 'src/*.html',
+    js: 'src/js/**/*.js',
   },
 };
 
@@ -51,15 +56,31 @@ function buildHTML(done) {
   done();
 }
 
+// Build JS
+function buildJS(done) {
+  gulp.src(dirs.src.js)
+    .pipe(uglify())
+    .pipe(rename({
+      suffix: ".min",
+      extname: ".js"
+    }))
+    .pipe(gulp.dest(dirs.dist.js));
+  done();
+}
+
 // Watch files
 function watch() {
-  gulp.watch(dirs.watch.html, gulp.series(buildHTML, reloadServer))
+  gulp.watch(dirs.watch.html, gulp.series(buildHTML, reloadServer));
+  gulp.watch(dirs.watch.js, gulp.series(buildJS, reloadServer));
 }
 
 // Export tasks
 exports.server = server;
 exports.watch = watch;
 exports.buildHTML = buildHTML;
+exports.buildJS = buildJS;
+const build = gulp.parallel(buildHTML, buildJS);
+exports.build = build;
 
 // Default task
-exports.default = gulp.series(buildHTML, server, watch);
+exports.default = gulp.series(build, server, watch);
